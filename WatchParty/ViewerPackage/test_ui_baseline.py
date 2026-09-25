daf915da8febfbe65bdd6238c3bd56cf4e21e6a5
xyqmbyl <2874724606@qq.com>
2026-09-25 16:19:53 +0800

    Scope bundled Chinese font checks to macOS packages

--- WatchParty/ViewerPackage/test_ui_baseline.py
+++ WatchParty/ViewerPackage/test_ui_baseline.py
@@ -56,7 +56,6 @@ V03_UOSC = {
 }
 
 V03_FONTS = {
-    "LXGWWenKaiMonoLite-Regular.ttf": "03d04443c99a261c5d1ac5cca1ef3e174194a5e1126d05f7a19c33617eb183f3",
     "MaterialIconsRound-Regular.otf": "bad85e5454b6288104ce03806c37323bcd8f145e3094e727860173ac8c91062e",
     "uosc_textures.ttf": "ccc0660f284dfceb5ab31eb363ccb2355df30fcdf628e781ee374b7d4172ada5",
 }
@@ -179,6 +178,10 @@ class UiBaselineTests(unittest.TestCase):
                 self.assertEqual(sha256(portable / "fonts" / name), expected)
         self.assertEqual(parse_conf(portable / "mpv.conf").get("osc"), "no")
         if (root / "mpv.app").is_dir():
+            self.assertEqual(
+                sha256(portable / "fonts/LXGWWenKaiMonoLite-Regular.ttf"),
+                "03d04443c99a261c5d1ac5cca1ef3e174194a5e1126d05f7a19c33617eb183f3",
+            )
             app = root / "mpv.app"
             info = plistlib.loads((app / "Contents/Info.plist").read_bytes())
             self.assertEqual(info.get("CFBundleExecutable"), "watchparty-launcher")
